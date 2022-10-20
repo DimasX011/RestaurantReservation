1bfdd52da9c5952b8c9218fcee4beded2dc2db58
Dmitry Yakovlev <dimon1998daf@mail.ru>
2022-10-20 09:34:05 +0600

    Lesson1

---
+++ Lesson1/TableReservation/TableReservation/State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableReservation
+{
+    public enum State
+    {
+        Free = 0,
+        Blocked = 1
+    }
+}
